f9489eeee313695828902c310cd8584015fa88e9
Zezhang Yang <yangzezhang95@gmail.com>
2025-10-16 23:30:58 -0500

    Delete reconstruction directory

--- reconstruction/mod_15/my_fwrite.m
+++
@@ -1,7 +0,0 @@
-function my_fwrite(filename, data, dataType)
-
-fid = fopen(filename,'wb');
-fwrite(fid, data, dataType);
-fclose(fid);
-
-end
--- reconstruction/mod_30_1/my_fread.m
+++
@@ -1,7 +0,0 @@
-function out = my_fread(filename, dataShape, dataType)
-
-fid = fopen(filename,'rb');
-out = fread(fid,dataShape,dataType);
-fclose(fid);
-
-end
